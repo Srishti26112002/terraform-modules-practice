58051d2ddf47d2008acb451d70359bcfcd839943
Srishti26112002 <167313917+Srishti26112002@users.noreply.github.com>
2024-05-27 14:01:43 +0530

    eks

--- eks/data.tf
+++ eks/data.tf
@@ -1,3 +1,5 @@
+# data source is used to fetch information
+
 data "aws_region" "current" {}
 
 data "aws_key_pair" "ssh_key_pair" {
@@ -16,12 +18,16 @@ data "aws_vpc" "vpc" {
   }
 }
 
-data "aws_subnets" "private_subnets" {
+
+data "aws_subnets" "subnets" {
+
+  # This filter ensures that only subnets within the specified VPC are retrieved.
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.vpc.id]
   }
 
+  # This filter selects subnets that have a specific tag with a given value. here subnet_type "private" tag is only filtered
   filter {
     name   = "tag:${var.subnet_tag}"
     values = [var.subnet_tag_value_private]
